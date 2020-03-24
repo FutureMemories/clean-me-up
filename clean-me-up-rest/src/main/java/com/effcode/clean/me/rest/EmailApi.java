bbb197aa174ab36cc0897f0b80d2688aea6804f8
Martin <teknocide@gmail.com>
2020-03-24 14:24:01 +0100

    only allow POST-messages

--- clean-me-up-rest/src/main/java/com/effcode/clean/me/rest/EmailApi.java
+++ clean-me-up-rest/src/main/java/com/effcode/clean/me/rest/EmailApi.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,7 +14,7 @@ public class EmailApi {
     @Autowired
     EmailHandler emailHandler;
 
-    @RequestMapping("/")
+    @PostMapping("/")
     public ResponseEntity<Void> send(@RequestParam String adr, @RequestParam String subject,
                                      @RequestParam String content) {
         boolean state = emailHandler.send(adr, subject, content);
--- clean-me-up-rest/src/test/java/com/effcode/clean/me/rest/EmailApiTest.java
+++ clean-me-up-rest/src/test/java/com/effcode/clean/me/rest/EmailApiTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -29,5 +30,14 @@ class EmailApiTest {
     @Test
     void happyPath() throws Exception {
         mockMvc.perform(post(VALID_REQUEST_URL)).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldOnlyAcceptPostRequests() throws Exception {
+        mockMvc.perform(delete(VALID_REQUEST_URL)).andExpect(status().isMethodNotAllowed());
+        mockMvc.perform(get(VALID_REQUEST_URL)).andExpect(status().isMethodNotAllowed());
+        mockMvc.perform(head(VALID_REQUEST_URL)).andExpect(status().isMethodNotAllowed());
+        mockMvc.perform(patch(VALID_REQUEST_URL)).andExpect(status().isMethodNotAllowed());
+        mockMvc.perform(put(VALID_REQUEST_URL)).andExpect(status().isMethodNotAllowed());
     }
 }
